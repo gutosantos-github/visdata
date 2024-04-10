9c40234d16981e0b827db2ca6f2189cc8ac76438
Augusto <gutosantos.ufrj@gmail.com>
2024-04-10 15:20:24 -0300

    ainda exercitando

--- cursoJavaScript/aulas/2-javascript_guanabara/1-Exercicios/2-exerc002/script.js
+++ cursoJavaScript/aulas/2-javascript_guanabara/1-Exercicios/2-exerc002/script.js
@@ -4,19 +4,24 @@ function verificar(){
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
 
-    if (fano.ariaValueMax.length == 0 || fano.value > ano){
+    if (fano.value.length == 0 || fano.value > ano){
         window.alert('Verifique os dados e tente novamente.')
     }else{
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada ${idade}`
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 10)
+                img.setAttribute('src', 'menino.jpg')
         }else{
             genero = 'Mulher'
         }
         res.style.textAline = 'center'
         res.innerHTML = `Detectamos ${genero} com idade de ${idade} anos.`
+        res.appendChild(img)
     }
 }
