8e8842426eb834064db1a8c45a2c63a3c42db844
Augusto <gutosantos.ufrj@gmail.com>
2024-04-12 08:31:06 -0300

    Fixando JS

--- cursoJavaScript/aulas/2-javascript_guanabara/1-Exercicios/3-exerc003/script.js
+++ cursoJavaScript/aulas/2-javascript_guanabara/1-Exercicios/3-exerc003/script.js
@@ -4,8 +4,11 @@ function contar(){
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
-
+    let res = document.getElementById('res')
+    
     if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
-        window.alert('Erro! Faltam dados.')
-    }    
+        alert('Erro! Faltam dados.')
+    }else{
+        res.innerHTML = 'Contando..'
+    }
 }
